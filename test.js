f6426791c39074318766c73816413212d8f7aab0
TheDudeOVerThere <glee12121212@gmail.com>
2024-10-26 22:26:58 +1100

    Trying to submit form data to js

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=-33.8&lon=151.68&appid=39e5d13ceac778eaf7fc4c93d4f097f5";
+const form = document.getElementById("test");
 
-fetch(url).then(response => response.json()).then(data => console.log(data));
+form?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    getWeather();
+});
+
+
+function getWeather() {
+    let latitude = document.getElementById("#lat");
+    let longitude = document.getElementById("#lon");
+    console.log("fdsa");
+    alert("boo");
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=39e5d13ceac778eaf7fc4c93d4f097f5";
+
+    fetch(url).then(response => response.json()).then(data => document.getElementById("report").textContent = JSON.stringify(data, undefined, 2));
+}
